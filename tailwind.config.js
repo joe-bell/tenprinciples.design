13535a5826a75ae9ea0d81a188b76f97c853b143
joe-bell <joe-bell@users.noreply.github.com>
2022-09-05 18:15:46 +0700

    chore: refine design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        braun: "#e99d39",
+      },
+    },
   },
   plugins: [],
 };
