5a57672b78d703738ebd7a386e60a82439b7ddde
joe-bell <joe-bell@users.noreply.github.com>
2022-09-05 16:25:19 +0700

    chore: inline style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-});
+export default defineConfig({});
