0f186d12c18c7d183e24bb69344b2c884f76d2c5
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-04-21 15:36:53 +0300

    chore: update infra

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,11 @@
 const config = {
-  "*": (filenames) => [
-    `prettier --ignore-unknown --no-error-on-unmatched-pattern --write ${filenames
+  "**/*": (filenames) => [
+    `pnpm prettier --ignore-unknown --no-error-on-unmatched-pattern -w -- ${filenames
       .map((filename) => `'${filename}'`)
       .join(" ")}`,
   ],
+  "*.astro": () => "pnpm astro check",
+  "*.{astro,mdx,cjs,mjs,js,jsx,svelte,ts,tsx,vue}": () => "pnpm tsc",
 };
 
 export default config;
